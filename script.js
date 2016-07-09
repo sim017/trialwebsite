8a401d238170ea2764da1e612eba4a58884ae99c
SIMRAN BAWKAR <SIMRAN BAWKAR>
2016-07-09 22:26:50 +0530

    first site

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+	what();
+	function what()
+{
+document.getElementById("nav").innerHTML=
+ '<ul>'+
+  '<li><a href="index.html">Home</a></li>'+
+ '<li ><a href="news.html">News</a></li>'+
+  '<li ><a href="contact.html">Contact</a></li>'+
+  '<li ><a href="about.html">About</a></li>'+
+ '</ul>';}
+}
